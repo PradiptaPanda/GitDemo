f756861cd5c394873fe073eb99e1a1fc6ea8bae1
pradipta <pradipta.panda31@gmail.com>
2021-06-26 12:14:35 +0530

    this is committed from Asian Timezone Guy

--- src/test/java/qaclickacademy/AppTest.java
+++ src/test/java/qaclickacademy/AppTest.java
@@ -18,6 +18,8 @@ public class AppTest
     public AppTest( String testName )
     {
         super( testName );
+	System.out.println("java project1")
+	System.out.println("java project2")
     }
 
     /**
